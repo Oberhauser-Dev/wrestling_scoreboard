f772daa0f53b3fde51020903b2293e30bbdafba0
Gustl22 <user.rebo@gmx.de>
2021-06-23 16:41:03 +0200

    📦 [Server] Default shelf server

--- server/lib/server.dart
+++ server/lib/server.dart
@@ -1,3 +1,67 @@
-int calculate() {
-  return 6 * 7;
+// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart' as shelf_router;
+import 'package:shelf_static/shelf_static.dart' as shelf_static;
+
+Future init() async {
+  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
+  // https://cloud.google.com/run/docs/reference/container-contract#port
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  // See https://pub.dev/documentation/shelf/latest/shelf/Cascade-class.html
+  final cascade = Cascade()
+  // First, serve files from the 'public' directory
+      .add(_staticHandler)
+  // If a corresponding file is not found, send requests to a `Router`
+      .add(_router);
+
+  // See https://pub.dev/documentation/shelf/latest/shelf/Pipeline-class.html
+  final pipeline = Pipeline()
+  // See https://pub.dev/documentation/shelf/latest/shelf/logRequests.html
+      .addMiddleware(logRequests())
+      .addHandler(cascade.handler);
+
+  // See https://pub.dev/documentation/shelf/latest/shelf_io/serve.html
+  final server = await shelf_io.serve(
+    pipeline,
+    InternetAddress.anyIPv4, // Allows external connections
+    port,
+  );
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+// Serve files from the file system.
+final _staticHandler =
+shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
+
+// Router instance to handler requests.
+final _router = shelf_router.Router()
+  ..get('/helloworld', _helloWorldHandler)
+  ..get(
+    '/time',
+        (request) => Response.ok(DateTime.now().toUtc().toIso8601String()),
+  )
+  ..get('/sum/<a|[0-9]+>/<b|[0-9]+>', _sumHandler);
+
+Response _helloWorldHandler(Request request) => Response.ok('Hello, World!');
+
+Response _sumHandler(request, String a, String b) {
+  final aNum = int.parse(a);
+  final bNum = int.parse(b);
+  return Response.ok(
+    const JsonEncoder.withIndent(' ')
+        .convert({'a': aNum, 'b': bNum, 'sum': aNum + bNum}),
+    headers: {
+      'content-type': 'application/json',
+      'Cache-Control': 'public, max-age=604800',
+    },
+  );
 }
