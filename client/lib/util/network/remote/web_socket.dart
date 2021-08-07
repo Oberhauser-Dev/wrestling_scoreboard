ae30de07f64b40e2e93f3b7aa19c47dfb913c140
Gustl22 <user.rebo@gmx.de>
2021-08-07 22:04:36 +0200

    🐛 [Client] Fix connecting to websocket only with trailing slash

--- client/lib/util/network/remote/web_socket.dart
+++ client/lib/util/network/remote/web_socket.dart
@@ -3,7 +3,8 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 final _wsUrl = dotenv.env['WEB_SOCKET_URL'] ?? 'ws://localhost:8080/ws';
 
-final _channel = WebSocketChannel.connect(Uri.parse(_wsUrl));
+// TODO try removing backslash if https://github.com/google/dart-neats/pull/146 is merged.
+final _channel = WebSocketChannel.connect(Uri.parse(_wsUrl.endsWith('/') ? _wsUrl : (_wsUrl + '/')));
 
 dynamic addToSink(String val) {
   _channel.sink.add(val);
