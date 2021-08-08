04eb4346115855e8956481f05eea8ced22d8f450
Gustl22 <user.rebo@gmx.de>
2021-08-08 23:11:40 +0200

    👌 [Server] Improve Web Socket Logging

--- server/lib/controllers/websocket_handler.dart
+++ server/lib/controllers/websocket_handler.dart
@@ -16,6 +16,7 @@ void broadcast(String data) {
 
 final websocketHandler = webSocketHandler((WebSocketChannel webSocket) {
   final handleSingle = ({required CRUD operation, required DataObject single}) async {
+    print('${DateTime.now()} ${operation.name.toUpperCase()} ${single.tableName}/${single.id}');
     final controller = EntityController.getControllerFromDataType(single.runtimeType);
     if (operation == CRUD.update) {
       await controller.updateSingle(single);
@@ -45,6 +46,7 @@ final websocketHandler = webSocketHandler((WebSocketChannel webSocket) {
     }
   };
   final handleMany = ({required CRUD operation, required ManyDataObject many}) {
+    print('${DateTime.now()} ${operation.name.toUpperCase()} ${getTableNameFromType(many.data.first.runtimeType)}s');
     throw DataUnimplementedError(operation, many.runtimeType);
   };
 
