d839c39bc2b7572d1d7f8124404fa299f4e6fae6
Gustl22 <git@reb0.org>
2022-01-04 15:57:00 +0100

    📦 Upgrade dependencies

--- client/lib/util/network/remote/web_socket.dart
+++ client/lib/util/network/remote/web_socket.dart
@@ -4,8 +4,7 @@ import 'package:wrestling_scoreboard/util/network/remote/url.dart';
 
 final _wsUrl = adaptLocalhost(env(webSocketUrl, fallBack: 'ws://localhost:8080/ws'));
 
-// TODO try removing backslash if https://github.com/google/dart-neats/pull/146 is merged.
-final _channel = WebSocketChannel.connect(Uri.parse(_wsUrl.endsWith('/') ? _wsUrl : (_wsUrl + '/')));
+final _channel = WebSocketChannel.connect(Uri.parse(_wsUrl));
 
 dynamic addToSink(String val) {
   _channel.sink.add(val);
