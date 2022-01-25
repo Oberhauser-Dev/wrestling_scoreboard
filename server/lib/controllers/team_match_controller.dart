8299a1942fb0d14299df7b1cba020de45dd008a9
Gustl22 <git@reb0.org>
2022-01-26 00:03:47 +0100

    🐛 [Server] Fix pos and missing id of generated fights

--- server/lib/controllers/team_match_controller.dart
+++ server/lib/controllers/team_match_controller.dart
@@ -7,6 +7,7 @@ import 'package:server/controllers/participant_state_controller.dart';
 import 'package:server/controllers/participation_controller.dart';
 import 'package:server/controllers/team_match_fight_controller.dart';
 import 'package:server/controllers/websocket_handler.dart';
+import 'package:server/services/postgres_db.dart';
 import 'package:shelf/shelf.dart';
 
 import 'entity_controller.dart';
@@ -41,7 +42,9 @@ class TeamMatchController extends EntityController<TeamMatch> {
     final isReset = (request.url.queryParameters['reset'] ?? '').parseBool();
     final teamMatch = (await getSingle(int.parse(id)))!;
     final oldFights = (await getFights(id)); // TODO Check if works...
-    final weightClasses = (await LeagueController().getWeightClasses(teamMatch.league.id.toString())); // TODO Check if works...
+    final weightClasses = teamMatch.league.id == null
+        ? <WeightClass>[]
+        : (await LeagueController().getWeightClasses(teamMatch.league.id.toString()));
     if (isReset) {
       await Future.forEach(oldFights, (Fight e) async {
         if (e.id != null) await deleteSingle(e.id!);
@@ -51,39 +54,47 @@ class TeamMatchController extends EntityController<TeamMatch> {
         .getMany(conditions: ['lineup_id = @id'], substitutionValues: {'id': teamMatch.home.id});
     final guestParticipations = await ParticipationController()
         .getMany(conditions: ['lineup_id = @id'], substitutionValues: {'id': teamMatch.guest.id});
-    
-    final fights = await teamMatch.generateFights([homeParticipations, guestParticipations], weightClasses);
-    
-    broadcast(
-        jsonEncode(manyToJson(fights, Fight, CRUD.update, filterType: TeamMatch, filterId: teamMatch.id)));
-    await Future.forEach(fights.asMap().entries, (MapEntry<int, Fight> entry) async {
-      final e = entry.value;
-      final hasRed = e.r != null;
-      final hasBlue = e.b != null;
+
+    final newFights = await teamMatch.generateFights([homeParticipations, guestParticipations], weightClasses);
+    final fights = List.of(newFights);
+    await Future.forEach(newFights.asMap().entries, (MapEntry<int, Fight> entry) async {
+      final fight = entry.value;
+      final hasRed = fight.r != null;
+      final hasBlue = fight.b != null;
       // Get fight of teamMatch that has the same participants and the same weightClass
       final res = await FightController().getManyRawFromQuery('''
-        SELECT f.id
+        SELECT f.*
         FROM fight AS f
         JOIN team_match_fight AS tmf ON f.id = tmf.fight_id
         ${hasRed ? 'JOIN participant_state AS ps_red ON ps_red.id = f.red_id' : ''}
         ${hasBlue ? 'JOIN participant_state AS ps_blue ON ps_blue.id = f.blue_id' : ''}
-        WHERE f.weight_class_id = ${e.weightClass.id}
+        WHERE f.weight_class_id = ${fight.weightClass.id}
         AND tmf.team_match_id = ${teamMatch.id}
-        AND ${hasRed ? 'ps_red.participation_id = ${e.r!.participation.id}' : 'f.red_id IS NULL'}
-        AND ${hasBlue ? 'ps_blue.participation_id = ${e.b!.participation.id}' : 'f.blue_id IS NULL'};
+        AND ${hasRed ? 'ps_red.participation_id = ${fight.r!.participation.id}' : 'f.red_id IS NULL'}
+        AND ${hasBlue ? 'ps_blue.participation_id = ${fight.b!.participation.id}' : 'f.blue_id IS NULL'};
         ''');
       if (res.isEmpty) {
         // Create ParticipantState to be stored in the team match fight
-        if (e.r != null) {
-          e.r!.id = await ParticipantStateController().createSingle(e.r!);
+        if (fight.r != null) {
+          fight.r!.id = await ParticipantStateController().createSingle(fight.r!);
         }
-        if (e.b != null) {
-          e.b!.id = await ParticipantStateController().createSingle(e.b!);
+        if (fight.b != null) {
+          fight.b!.id = await ParticipantStateController().createSingle(fight.b!);
         }
-        e.id = await FightController().createSingle(e);
-        await TeamMatchFightController().createSingle(TeamMatchFight(teamMatch: teamMatch, fight: e, pos: entry.key));
+        fight.id = await FightController().createSingle(fight);
+        await TeamMatchFightController()
+            .createSingle(TeamMatchFight(teamMatch: teamMatch, fight: fight, pos: entry.key));
+        fights[entry.key] = fight;
+      } else {
+        fight.id = res.first['id'];
+        fights[entry.key] = await Fight.fromRaw(res.first, EntityController.getSingleFromDataType);
+        await PostgresDb()
+            .connection
+            .query('UPDATE team_match_fight SET pos = ${entry.key} WHERE fight_id = ${fight.id};');
       }
     });
+    broadcast(jsonEncode(manyToJson(fights, Fight, CRUD.update, filterType: TeamMatch, filterId: teamMatch.id)));
+
     return Response.ok('{"status": "success"}');
   }
 
