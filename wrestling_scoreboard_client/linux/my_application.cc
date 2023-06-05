81e813cb69b843476d4c797775d16e7e4d925a51
Gustl22 <git@reb0.org>
2023-06-06 01:27:34 +0200

    fix(linux): Use correct application name

--- wrestling_scoreboard_client/linux/my_application.cc
+++ wrestling_scoreboard_client/linux/my_application.cc
@@ -40,12 +40,12 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "wrestling_scoreboard");
+    gtk_header_bar_set_title(header_bar, "Wrestling Scoreboard Client");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else {
-    gtk_window_set_title(window, "wrestling_scoreboard");
+    gtk_window_set_title(window, "Wrestling Scoreboard Client");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
