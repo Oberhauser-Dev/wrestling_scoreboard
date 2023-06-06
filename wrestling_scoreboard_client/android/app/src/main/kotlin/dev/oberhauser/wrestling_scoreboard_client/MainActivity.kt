bf5925d870ad4625b51cf1e0ca72cff8cea425b6
Gustl22 <git@reb0.org>
2023-06-06 13:33:52 +0200

    chore: flutter create

---
+++ wrestling_scoreboard_client/android/app/src/main/kotlin/dev/oberhauser/wrestling_scoreboard_client/MainActivity.kt
@@ -0,0 +1,6 @@
+ackage dev.oberhauser.wrestling_scoreboard_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wrestling_scoreboard_client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import path_provider_foundation
+import printing
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
