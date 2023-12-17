008f88079ee48ae01332a104e266028b41ad0fb6
Gustl22 <git@reb0.org>
2023-12-17 01:24:11 +0100

    feat: Show Changelog in app

--- wrestling_scoreboard_client/lib/ui/more/about.dart
+++ wrestling_scoreboard_client/lib/ui/more/about.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:wrestling_scoreboard_client/main.dart';
 import 'package:wrestling_scoreboard_client/ui/components/responsive_container.dart';
 
+const loadCompleteChangelog = false;
+
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
+
+  Future<String> _loadChangelog() async {
+    if (loadCompleteChangelog) {
+      final globalChangelogUrl =
+          Uri.https('raw.githubusercontent.com', '/Oberhauser-Dev/wrestling_scoreboard/main/CHANGELOG.md');
+      return http.read(globalChangelogUrl);
+    } else {
+      return rootBundle.loadString('CHANGELOG.md');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,9 +58,33 @@ class AboutScreen extends StatelessWidget {
                     ListTile(
                       leading: const Icon(Icons.list),
                       title: Text(localizations.about_Changelog),
-                      onTap: () => launchUrl(Uri.parse(
-                        'https://github.com/Oberhauser-Dev/wrestling_scoreboard/blob/main/CHANGELOG.md',
-                      )),
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          actions: [
+                            TextButton(
+                              child: Text(localizations.ok),
+                              onPressed: () => Navigator.of(context).pop(),
+                            ),
+                          ],
+                          content: FutureBuilder(
+                            future: _loadChangelog(),
+                            builder: (context, snapshot) {
+                              return snapshot.hasData
+                                  ? SingleChildScrollView(
+                                      child: MarkdownBody(
+                                        listItemCrossAxisAlignment: MarkdownListItemCrossAxisAlignment.start,
+                                        shrinkWrap: true,
+                                        selectable: true,
+                                        data: snapshot.data!,
+                                        onTapLink: (text, href, title) => launchUrl(Uri.parse(href!)),
+                                      ),
+                                    )
+                                  : const Center(child: CircularProgressIndicator());
+                            },
+                          ),
+                        ),
+                      ),
                     ),
                     const Divider(),
                     ListTile(
