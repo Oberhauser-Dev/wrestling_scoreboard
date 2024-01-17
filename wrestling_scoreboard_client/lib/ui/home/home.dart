edfaa001ab794869947bb5e33e9acb7a747408b1
Gustl22 <git@reb0.org>
2024-01-17 15:46:36 +0100

    refactor: Split clubs and leagues view from home

--- wrestling_scoreboard_client/lib/ui/home/home.dart
+++ wrestling_scoreboard_client/lib/ui/home/home.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:wrestling_scoreboard_client/provider/network_provider.dart';
-import 'package:wrestling_scoreboard_client/ui/components/consumer.dart';
-import 'package:wrestling_scoreboard_client/ui/components/grouped_list.dart';
-import 'package:wrestling_scoreboard_client/ui/components/responsive_container.dart';
-import 'package:wrestling_scoreboard_client/ui/edit/club_edit.dart';
-import 'package:wrestling_scoreboard_client/ui/edit/team_match/league_edit.dart';
-import 'package:wrestling_scoreboard_client/ui/overview/club_overview.dart';
-import 'package:wrestling_scoreboard_client/ui/overview/team_match/league_overview.dart';
+import 'package:wrestling_scoreboard_client/ui/home/clubs_view.dart';
+import 'package:wrestling_scoreboard_client/ui/home/leagues_view.dart';
 import 'package:wrestling_scoreboard_client/util/network/remote/web_socket.dart';
-import 'package:wrestling_scoreboard_common/common.dart';
 
 class Home extends ConsumerStatefulWidget {
   const Home({super.key});
@@ -101,76 +94,13 @@ class HomeState extends ConsumerState<Home> {
             ],
           ),
         ),
-        body: TabBarView(
+        body: const TabBarView(
           children: [
-            ResponsiveScrollView(
-              child: ManyConsumer<Club, Null>(
-                builder: (BuildContext context, List<Club> clubs) {
-                  return ListGroup(
-                    header: HeadingItem(
-                      title: localizations.clubs,
-                      trailing: IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () =>
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => const ClubEdit())),
-                      ),
-                    ),
-                    items: clubs.map(
-                      (e) => SingleConsumer<Club>(
-                        id: e.id!,
-                        initialData: e,
-                        builder: (context, data) {
-                          return ContentItem(
-                            title: data.name,
-                            icon: Icons.foundation,
-                            onTap: () => handleSelectedClub(data, context),
-                          );
-                        },
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            ResponsiveScrollView(
-              child: ManyConsumer<League, Null>(
-                builder: (BuildContext context, List<League> leagues) {
-                  return ListGroup(
-                    header: HeadingItem(
-                      title: localizations.leagues,
-                      trailing: IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => LeagueEdit())),
-                      ),
-                    ),
-                    items: leagues.map(
-                      (e) => SingleConsumer<League>(
-                        id: e.id!,
-                        initialData: e,
-                        builder: (context, data) {
-                          return ContentItem(
-                            title: data.name,
-                            icon: Icons.emoji_events,
-                            onTap: () => handleSelectedLeague(data, context),
-                          );
-                        },
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+            LeaguesView(),
+            ClubsView(),
           ],
         ),
       ),
     );
-  }
-
-  handleSelectedClub(Club club, BuildContext context) {
-    context.push('/${ClubOverview.route}/${club.id}');
-  }
-
-  handleSelectedLeague(League league, BuildContext context) {
-    context.push('/${LeagueOverview.route}/${league.id}');
   }
 }
