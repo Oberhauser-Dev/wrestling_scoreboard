7dce8d53cd9fa7f7815fbfda2b658e6e50100eee
Gustl22 <git@reb0.org>
2025-04-21 10:01:12 +0200

    type tests

--- wrestling_scoreboard_common/test/data/data_object_test.dart
+++ wrestling_scoreboard_common/test/data/data_object_test.dart
@@ -4,7 +4,7 @@ import 'package:wrestling_scoreboard_common/common.dart';
 // ignore_for_file: avoid_print
 
 void main() {
-  test('Sync data object types', () {
+  test('Conversion of data types to table names', () {
     for (final dataType in dataTypes) {
       expect(dataType, getTypeFromTableName(getTableNameFromType(dataType)));
     }
@@ -12,5 +12,35 @@ void main() {
     final sortedTableNames =
         dataTypes.map((dataType) => getTableNameFromType(dataType)).sorted((a, b) => a.compareTo(b));
     print(sortedTableNames.indexed.map((e) => '${e.$1} ${e.$2}').join('\n'));
+  });
+
+  test('Handle generic json', () async {
+    Future<int> handleSingle<T extends DataObject>({required CRUD operation, required T single}) async {
+      return single.id!;
+    }
+
+    Future<int> handleSingleRaw<T extends DataObject>({
+      required CRUD operation,
+      required Map<String, dynamic> single,
+    }) async {
+      return single['id'];
+    }
+
+    Future<void> handleMany<T extends DataObject>({required CRUD operation, required ManyDataObject<T> many}) async {}
+
+    Future<void> handleManyRaw<T extends DataObject>({
+      required CRUD operation,
+      required ManyDataObject<Map<String, dynamic>> many,
+    }) async {}
+
+    for (final dataType in dataTypes) {
+      await handleGenericJson(
+        singleToJson({'id': 0}, dataType, CRUD.read),
+        handleSingle: handleSingle,
+        handleMany: handleMany,
+        handleSingleRaw: handleSingleRaw,
+        handleManyRaw: handleManyRaw,
+      );
+    }
   });
 }
