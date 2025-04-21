ab5e1d5db47818c9253a59ed6ac20d364f8d4b8e
Gustl22 <git@reb0.org>
2025-04-21 09:37:02 +0200

    add types

--- wrestling_scoreboard_common/test/data/data_object_test.dart
+++ wrestling_scoreboard_common/test/data/data_object_test.dart
@@ -1,10 +1,15 @@
+import 'package:collection/collection.dart';
 import 'package:test/test.dart';
 import 'package:wrestling_scoreboard_common/common.dart';
+// ignore_for_file: avoid_print
 
 void main() {
   test('Sync data object types', () {
-    for (final dataType in dataTypes) {
-
-    }
+    print(dataTypes
+        .map((dataType) => getTableNameFromType(dataType))
+        .sorted((a, b) => a.compareTo(b))
+        .indexed
+        .map((e) => '${e.$1} ${e.$2}')
+        .join('\n'));
   });
 }
