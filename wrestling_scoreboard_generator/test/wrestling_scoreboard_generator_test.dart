437d07caeb4fe8231b2faee5cd6f61cc0bda702a
Gustl22 <git@reb0.org>
2025-04-24 07:45:59 +0200

    Create package generator

---
+++ wrestling_scoreboard_generator/test/wrestling_scoreboard_generator_test.dart
@@ -0,0 +1,16 @@
+import 'package:wrestling_scoreboard_generator/generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
