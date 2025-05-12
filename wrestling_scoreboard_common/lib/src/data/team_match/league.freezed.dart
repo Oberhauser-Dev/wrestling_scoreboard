368d62a7b2dcdef48abba98f7e4ddaeb437c9a80
Gustl22 <git@reb0.org>
2025-05-12 21:45:29 +0200

    refactor: Dart format

--- wrestling_scoreboard_common/lib/src/data/team_match/league.freezed.dart
+++ wrestling_scoreboard_common/lib/src/data/team_match/league.freezed.dart
@@ -15,329 +15,201 @@ T _$identity<T>(T value) => value;
 
 /// @nodoc
 mixin _$League {
-  int? get id;
-  String? get orgSyncId;
-  Organization? get organization;
-  String get name;
-  DateTime get startDate;
-  DateTime get endDate;
-  Division get division;
 
-  /// The bout days are not necessarily the total days a league has, but ideally they should.
-  /// More binding is the seasonPartition of each single Match.
-  int get boutDays;
-
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @pragma('vm:prefer-inline')
-  $LeagueCopyWith<League> get copyWith => _$LeagueCopyWithImpl<League>(this as League, _$identity);
+ int? get id; String? get orgSyncId; Organization? get organization; String get name; DateTime get startDate; DateTime get endDate; Division get division;/// The bout days are not necessarily the total days a league has, but ideally they should.
+/// More binding is the seasonPartition of each single Match.
+ int get boutDays;
+/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$LeagueCopyWith<League> get copyWith => _$LeagueCopyWithImpl<League>(this as League, _$identity);
 
   /// Serializes this League to a JSON map.
   Map<String, dynamic> toJson();
 
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is League &&
-            (identical(other.id, id) || other.id == id) &&
-            (identical(other.orgSyncId, orgSyncId) || other.orgSyncId == orgSyncId) &&
-            (identical(other.organization, organization) || other.organization == organization) &&
-            (identical(other.name, name) || other.name == name) &&
-            (identical(other.startDate, startDate) || other.startDate == startDate) &&
-            (identical(other.endDate, endDate) || other.endDate == endDate) &&
-            (identical(other.division, division) || other.division == division) &&
-            (identical(other.boutDays, boutDays) || other.boutDays == boutDays));
-  }
 
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  int get hashCode =>
-      Object.hash(runtimeType, id, orgSyncId, organization, name, startDate, endDate, division, boutDays);
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is League&&(identical(other.id, id) || other.id == id)&&(identical(other.orgSyncId, orgSyncId) || other.orgSyncId == orgSyncId)&&(identical(other.organization, organization) || other.organization == organization)&&(identical(other.name, name) || other.name == name)&&(identical(other.startDate, startDate) || other.startDate == startDate)&&(identical(other.endDate, endDate) || other.endDate == endDate)&&(identical(other.division, division) || other.division == division)&&(identical(other.boutDays, boutDays) || other.boutDays == boutDays));
+}
 
-  @override
-  String toString() {
-    return 'League(id: $id, orgSyncId: $orgSyncId, organization: $organization, name: $name, startDate: $startDate, endDate: $endDate, division: $division, boutDays: $boutDays)';
-  }
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,orgSyncId,organization,name,startDate,endDate,division,boutDays);
+
+@override
+String toString() {
+  return 'League(id: $id, orgSyncId: $orgSyncId, organization: $organization, name: $name, startDate: $startDate, endDate: $endDate, division: $division, boutDays: $boutDays)';
+}
+
+
 }
 
 /// @nodoc
-abstract mixin class $LeagueCopyWith<$Res> {
+abstract mixin class $LeagueCopyWith<$Res>  {
   factory $LeagueCopyWith(League value, $Res Function(League) _then) = _$LeagueCopyWithImpl;
-  @useResult
-  $Res call(
-      {int? id,
-      String? orgSyncId,
-      Organization? organization,
-      String name,
-      DateTime startDate,
-      DateTime endDate,
-      Division division,
-      int boutDays});
+@useResult
+$Res call({
+ int? id, String? orgSyncId, Organization? organization, String name, DateTime startDate, DateTime endDate, Division division, int boutDays
+});
 
-  $OrganizationCopyWith<$Res>? get organization;
-  $DivisionCopyWith<$Res> get division;
+
+$OrganizationCopyWith<$Res>? get organization;$DivisionCopyWith<$Res> get division;
+
 }
-
 /// @nodoc
-class _$LeagueCopyWithImpl<$Res> implements $LeagueCopyWith<$Res> {
+class _$LeagueCopyWithImpl<$Res>
+    implements $LeagueCopyWith<$Res> {
   _$LeagueCopyWithImpl(this._self, this._then);
 
   final League _self;
   final $Res Function(League) _then;
 
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? id = freezed,
-    Object? orgSyncId = freezed,
-    Object? organization = freezed,
-    Object? name = null,
-    Object? startDate = null,
-    Object? endDate = null,
-    Object? division = null,
-    Object? boutDays = null,
-  }) {
-    return _then(_self.copyWith(
-      id: freezed == id
-          ? _self.id
-          : id // ignore: cast_nullable_to_non_nullable
-              as int?,
-      orgSyncId: freezed == orgSyncId
-          ? _self.orgSyncId
-          : orgSyncId // ignore: cast_nullable_to_non_nullable
-              as String?,
-      organization: freezed == organization
-          ? _self.organization
-          : organization // ignore: cast_nullable_to_non_nullable
-              as Organization?,
-      name: null == name
-          ? _self.name
-          : name // ignore: cast_nullable_to_non_nullable
-              as String,
-      startDate: null == startDate
-          ? _self.startDate
-          : startDate // ignore: cast_nullable_to_non_nullable
-              as DateTime,
-      endDate: null == endDate
-          ? _self.endDate
-          : endDate // ignore: cast_nullable_to_non_nullable
-              as DateTime,
-      division: null == division
-          ? _self.division
-          : division // ignore: cast_nullable_to_non_nullable
-              as Division,
-      boutDays: null == boutDays
-          ? _self.boutDays
-          : boutDays // ignore: cast_nullable_to_non_nullable
-              as int,
-    ));
-  }
-
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $OrganizationCopyWith<$Res>? get organization {
-    if (_self.organization == null) {
-      return null;
-    }
-
-    return $OrganizationCopyWith<$Res>(_self.organization!, (value) {
-      return _then(_self.copyWith(organization: value));
-    });
-  }
-
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $DivisionCopyWith<$Res> get division {
-    return $DivisionCopyWith<$Res>(_self.division, (value) {
-      return _then(_self.copyWith(division: value));
-    });
-  }
+/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = freezed,Object? orgSyncId = freezed,Object? organization = freezed,Object? name = null,Object? startDate = null,Object? endDate = null,Object? division = null,Object? boutDays = null,}) {
+  return _then(_self.copyWith(
+id: freezed == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int?,orgSyncId: freezed == orgSyncId ? _self.orgSyncId : orgSyncId // ignore: cast_nullable_to_non_nullable
+as String?,organization: freezed == organization ? _self.organization : organization // ignore: cast_nullable_to_non_nullable
+as Organization?,name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,startDate: null == startDate ? _self.startDate : startDate // ignore: cast_nullable_to_non_nullable
+as DateTime,endDate: null == endDate ? _self.endDate : endDate // ignore: cast_nullable_to_non_nullable
+as DateTime,division: null == division ? _self.division : division // ignore: cast_nullable_to_non_nullable
+as Division,boutDays: null == boutDays ? _self.boutDays : boutDays // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
 }
+/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$OrganizationCopyWith<$Res>? get organization {
+    if (_self.organization == null) {
+    return null;
+  }
+
+  return $OrganizationCopyWith<$Res>(_self.organization!, (value) {
+    return _then(_self.copyWith(organization: value));
+  });
+}/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$DivisionCopyWith<$Res> get division {
+  
+  return $DivisionCopyWith<$Res>(_self.division, (value) {
+    return _then(_self.copyWith(division: value));
+  });
+}
+}
+
 
 /// @nodoc
 @JsonSerializable()
+
 class _League extends League {
-  const _League(
-      {this.id,
-      this.orgSyncId,
-      this.organization,
-      required this.name,
-      required this.startDate,
-      required this.endDate,
-      required this.division,
-      required this.boutDays})
-      : super._();
+  const _League({this.id, this.orgSyncId, this.organization, required this.name, required this.startDate, required this.endDate, required this.division, required this.boutDays}): super._();
   factory _League.fromJson(Map<String, dynamic> json) => _$LeagueFromJson(json);
 
-  @override
-  final int? id;
-  @override
-  final String? orgSyncId;
-  @override
-  final Organization? organization;
-  @override
-  final String name;
-  @override
-  final DateTime startDate;
-  @override
-  final DateTime endDate;
-  @override
-  final Division division;
+@override final  int? id;
+@override final  String? orgSyncId;
+@override final  Organization? organization;
+@override final  String name;
+@override final  DateTime startDate;
+@override final  DateTime endDate;
+@override final  Division division;
+/// The bout days are not necessarily the total days a league has, but ideally they should.
+/// More binding is the seasonPartition of each single Match.
+@override final  int boutDays;
 
-  /// The bout days are not necessarily the total days a league has, but ideally they should.
-  /// More binding is the seasonPartition of each single Match.
-  @override
-  final int boutDays;
+/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$LeagueCopyWith<_League> get copyWith => __$LeagueCopyWithImpl<_League>(this, _$identity);
 
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @pragma('vm:prefer-inline')
-  _$LeagueCopyWith<_League> get copyWith => __$LeagueCopyWithImpl<_League>(this, _$identity);
+@override
+Map<String, dynamic> toJson() {
+  return _$LeagueToJson(this, );
+}
 
-  @override
-  Map<String, dynamic> toJson() {
-    return _$LeagueToJson(
-      this,
-    );
-  }
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _League&&(identical(other.id, id) || other.id == id)&&(identical(other.orgSyncId, orgSyncId) || other.orgSyncId == orgSyncId)&&(identical(other.organization, organization) || other.organization == organization)&&(identical(other.name, name) || other.name == name)&&(identical(other.startDate, startDate) || other.startDate == startDate)&&(identical(other.endDate, endDate) || other.endDate == endDate)&&(identical(other.division, division) || other.division == division)&&(identical(other.boutDays, boutDays) || other.boutDays == boutDays));
+}
 
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is _League &&
-            (identical(other.id, id) || other.id == id) &&
-            (identical(other.orgSyncId, orgSyncId) || other.orgSyncId == orgSyncId) &&
-            (identical(other.organization, organization) || other.organization == organization) &&
-            (identical(other.name, name) || other.name == name) &&
-            (identical(other.startDate, startDate) || other.startDate == startDate) &&
-            (identical(other.endDate, endDate) || other.endDate == endDate) &&
-            (identical(other.division, division) || other.division == division) &&
-            (identical(other.boutDays, boutDays) || other.boutDays == boutDays));
-  }
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,orgSyncId,organization,name,startDate,endDate,division,boutDays);
 
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  int get hashCode =>
-      Object.hash(runtimeType, id, orgSyncId, organization, name, startDate, endDate, division, boutDays);
+@override
+String toString() {
+  return 'League(id: $id, orgSyncId: $orgSyncId, organization: $organization, name: $name, startDate: $startDate, endDate: $endDate, division: $division, boutDays: $boutDays)';
+}
 
-  @override
-  String toString() {
-    return 'League(id: $id, orgSyncId: $orgSyncId, organization: $organization, name: $name, startDate: $startDate, endDate: $endDate, division: $division, boutDays: $boutDays)';
-  }
+
 }
 
 /// @nodoc
 abstract mixin class _$LeagueCopyWith<$Res> implements $LeagueCopyWith<$Res> {
   factory _$LeagueCopyWith(_League value, $Res Function(_League) _then) = __$LeagueCopyWithImpl;
-  @override
-  @useResult
-  $Res call(
-      {int? id,
-      String? orgSyncId,
-      Organization? organization,
-      String name,
-      DateTime startDate,
-      DateTime endDate,
-      Division division,
-      int boutDays});
+@override @useResult
+$Res call({
+ int? id, String? orgSyncId, Organization? organization, String name, DateTime startDate, DateTime endDate, Division division, int boutDays
+});
 
-  @override
-  $OrganizationCopyWith<$Res>? get organization;
-  @override
-  $DivisionCopyWith<$Res> get division;
+
+@override $OrganizationCopyWith<$Res>? get organization;@override $DivisionCopyWith<$Res> get division;
+
 }
-
 /// @nodoc
-class __$LeagueCopyWithImpl<$Res> implements _$LeagueCopyWith<$Res> {
+class __$LeagueCopyWithImpl<$Res>
+    implements _$LeagueCopyWith<$Res> {
   __$LeagueCopyWithImpl(this._self, this._then);
 
   final _League _self;
   final $Res Function(_League) _then;
 
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $Res call({
-    Object? id = freezed,
-    Object? orgSyncId = freezed,
-    Object? organization = freezed,
-    Object? name = null,
-    Object? startDate = null,
-    Object? endDate = null,
-    Object? division = null,
-    Object? boutDays = null,
-  }) {
-    return _then(_League(
-      id: freezed == id
-          ? _self.id
-          : id // ignore: cast_nullable_to_non_nullable
-              as int?,
-      orgSyncId: freezed == orgSyncId
-          ? _self.orgSyncId
-          : orgSyncId // ignore: cast_nullable_to_non_nullable
-              as String?,
-      organization: freezed == organization
-          ? _self.organization
-          : organization // ignore: cast_nullable_to_non_nullable
-              as Organization?,
-      name: null == name
-          ? _self.name
-          : name // ignore: cast_nullable_to_non_nullable
-              as String,
-      startDate: null == startDate
-          ? _self.startDate
-          : startDate // ignore: cast_nullable_to_non_nullable
-              as DateTime,
-      endDate: null == endDate
-          ? _self.endDate
-          : endDate // ignore: cast_nullable_to_non_nullable
-              as DateTime,
-      division: null == division
-          ? _self.division
-          : division // ignore: cast_nullable_to_non_nullable
-              as Division,
-      boutDays: null == boutDays
-          ? _self.boutDays
-          : boutDays // ignore: cast_nullable_to_non_nullable
-              as int,
-    ));
-  }
+/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = freezed,Object? orgSyncId = freezed,Object? organization = freezed,Object? name = null,Object? startDate = null,Object? endDate = null,Object? division = null,Object? boutDays = null,}) {
+  return _then(_League(
+id: freezed == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int?,orgSyncId: freezed == orgSyncId ? _self.orgSyncId : orgSyncId // ignore: cast_nullable_to_non_nullable
+as String?,organization: freezed == organization ? _self.organization : organization // ignore: cast_nullable_to_non_nullable
+as Organization?,name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,startDate: null == startDate ? _self.startDate : startDate // ignore: cast_nullable_to_non_nullable
+as DateTime,endDate: null == endDate ? _self.endDate : endDate // ignore: cast_nullable_to_non_nullable
+as DateTime,division: null == division ? _self.division : division // ignore: cast_nullable_to_non_nullable
+as Division,boutDays: null == boutDays ? _self.boutDays : boutDays // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
 
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $OrganizationCopyWith<$Res>? get organization {
+/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$OrganizationCopyWith<$Res>? get organization {
     if (_self.organization == null) {
-      return null;
-    }
-
-    return $OrganizationCopyWith<$Res>(_self.organization!, (value) {
-      return _then(_self.copyWith(organization: value));
-    });
+    return null;
   }
 
-  /// Create a copy of League
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $DivisionCopyWith<$Res> get division {
-    return $DivisionCopyWith<$Res>(_self.division, (value) {
-      return _then(_self.copyWith(division: value));
-    });
-  }
+  return $OrganizationCopyWith<$Res>(_self.organization!, (value) {
+    return _then(_self.copyWith(organization: value));
+  });
+}/// Create a copy of League
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$DivisionCopyWith<$Res> get division {
+  
+  return $DivisionCopyWith<$Res>(_self.division, (value) {
+    return _then(_self.copyWith(division: value));
+  });
+}
 }
 
 // dart format on
--- wrestling_scoreboard_common/lib/src/data/weight_class.freezed.dart
+++ wrestling_scoreboard_common/lib/src/data/weight_class.freezed.dart
@@ -15,166 +15,132 @@ T _$identity<T>(T value) => value;
 
 /// @nodoc
 mixin _$WeightClass {
-  int? get id;
-  int get weight;
-  WrestlingStyle get style;
-  String? get suffix;
-  WeightUnit get unit;
 
-  /// Create a copy of WeightClass
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @pragma('vm:prefer-inline')
-  $WeightClassCopyWith<WeightClass> get copyWith =>
-      _$WeightClassCopyWithImpl<WeightClass>(this as WeightClass, _$identity);
+ int? get id; int get weight; WrestlingStyle get style; String? get suffix; WeightUnit get unit;
+/// Create a copy of WeightClass
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$WeightClassCopyWith<WeightClass> get copyWith => _$WeightClassCopyWithImpl<WeightClass>(this as WeightClass, _$identity);
 
   /// Serializes this WeightClass to a JSON map.
   Map<String, dynamic> toJson();
 
-  @override
-  String toString() {
-    return 'WeightClass(id: $id, weight: $weight, style: $style, suffix: $suffix, unit: $unit)';
-  }
+
+
+
+@override
+String toString() {
+  return 'WeightClass(id: $id, weight: $weight, style: $style, suffix: $suffix, unit: $unit)';
+}
+
+
 }
 
 /// @nodoc
-abstract mixin class $WeightClassCopyWith<$Res> {
+abstract mixin class $WeightClassCopyWith<$Res>  {
   factory $WeightClassCopyWith(WeightClass value, $Res Function(WeightClass) _then) = _$WeightClassCopyWithImpl;
-  @useResult
-  $Res call({int? id, int weight, WrestlingStyle style, String? suffix, WeightUnit unit});
-}
+@useResult
+$Res call({
+ int? id, int weight, WrestlingStyle style, String? suffix, WeightUnit unit
+});
 
+
+
+
+}
 /// @nodoc
-class _$WeightClassCopyWithImpl<$Res> implements $WeightClassCopyWith<$Res> {
+class _$WeightClassCopyWithImpl<$Res>
+    implements $WeightClassCopyWith<$Res> {
   _$WeightClassCopyWithImpl(this._self, this._then);
 
   final WeightClass _self;
   final $Res Function(WeightClass) _then;
 
-  /// Create a copy of WeightClass
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? id = freezed,
-    Object? weight = null,
-    Object? style = null,
-    Object? suffix = freezed,
-    Object? unit = null,
-  }) {
-    return _then(_self.copyWith(
-      id: freezed == id
-          ? _self.id
-          : id // ignore: cast_nullable_to_non_nullable
-              as int?,
-      weight: null == weight
-          ? _self.weight
-          : weight // ignore: cast_nullable_to_non_nullable
-              as int,
-      style: null == style
-          ? _self.style
-          : style // ignore: cast_nullable_to_non_nullable
-              as WrestlingStyle,
-      suffix: freezed == suffix
-          ? _self.suffix
-          : suffix // ignore: cast_nullable_to_non_nullable
-              as String?,
-      unit: null == unit
-          ? _self.unit
-          : unit // ignore: cast_nullable_to_non_nullable
-              as WeightUnit,
-    ));
-  }
+/// Create a copy of WeightClass
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = freezed,Object? weight = null,Object? style = null,Object? suffix = freezed,Object? unit = null,}) {
+  return _then(_self.copyWith(
+id: freezed == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int?,weight: null == weight ? _self.weight : weight // ignore: cast_nullable_to_non_nullable
+as int,style: null == style ? _self.style : style // ignore: cast_nullable_to_non_nullable
+as WrestlingStyle,suffix: freezed == suffix ? _self.suffix : suffix // ignore: cast_nullable_to_non_nullable
+as String?,unit: null == unit ? _self.unit : unit // ignore: cast_nullable_to_non_nullable
+as WeightUnit,
+  ));
 }
+
+}
+
 
 /// @nodoc
 @JsonSerializable()
+
 class _WeightClass extends WeightClass {
-  const _WeightClass({this.id, required this.weight, required this.style, this.suffix, this.unit = WeightUnit.kilogram})
-      : super._();
+  const _WeightClass({this.id, required this.weight, required this.style, this.suffix, this.unit = WeightUnit.kilogram}): super._();
   factory _WeightClass.fromJson(Map<String, dynamic> json) => _$WeightClassFromJson(json);
 
-  @override
-  final int? id;
-  @override
-  final int weight;
-  @override
-  final WrestlingStyle style;
-  @override
-  final String? suffix;
-  @override
-  @JsonKey()
-  final WeightUnit unit;
+@override final  int? id;
+@override final  int weight;
+@override final  WrestlingStyle style;
+@override final  String? suffix;
+@override@JsonKey() final  WeightUnit unit;
 
-  /// Create a copy of WeightClass
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @pragma('vm:prefer-inline')
-  _$WeightClassCopyWith<_WeightClass> get copyWith => __$WeightClassCopyWithImpl<_WeightClass>(this, _$identity);
+/// Create a copy of WeightClass
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$WeightClassCopyWith<_WeightClass> get copyWith => __$WeightClassCopyWithImpl<_WeightClass>(this, _$identity);
 
-  @override
-  Map<String, dynamic> toJson() {
-    return _$WeightClassToJson(
-      this,
-    );
-  }
+@override
+Map<String, dynamic> toJson() {
+  return _$WeightClassToJson(this, );
+}
 
-  @override
-  String toString() {
-    return 'WeightClass(id: $id, weight: $weight, style: $style, suffix: $suffix, unit: $unit)';
-  }
+
+
+@override
+String toString() {
+  return 'WeightClass(id: $id, weight: $weight, style: $style, suffix: $suffix, unit: $unit)';
+}
+
+
 }
 
 /// @nodoc
 abstract mixin class _$WeightClassCopyWith<$Res> implements $WeightClassCopyWith<$Res> {
   factory _$WeightClassCopyWith(_WeightClass value, $Res Function(_WeightClass) _then) = __$WeightClassCopyWithImpl;
-  @override
-  @useResult
-  $Res call({int? id, int weight, WrestlingStyle style, String? suffix, WeightUnit unit});
-}
+@override @useResult
+$Res call({
+ int? id, int weight, WrestlingStyle style, String? suffix, WeightUnit unit
+});
 
+
+
+
+}
 /// @nodoc
-class __$WeightClassCopyWithImpl<$Res> implements _$WeightClassCopyWith<$Res> {
+class __$WeightClassCopyWithImpl<$Res>
+    implements _$WeightClassCopyWith<$Res> {
   __$WeightClassCopyWithImpl(this._self, this._then);
 
   final _WeightClass _self;
   final $Res Function(_WeightClass) _then;
 
-  /// Create a copy of WeightClass
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $Res call({
-    Object? id = freezed,
-    Object? weight = null,
-    Object? style = null,
-    Object? suffix = freezed,
-    Object? unit = null,
-  }) {
-    return _then(_WeightClass(
-      id: freezed == id
-          ? _self.id
-          : id // ignore: cast_nullable_to_non_nullable
-              as int?,
-      weight: null == weight
-          ? _self.weight
-          : weight // ignore: cast_nullable_to_non_nullable
-              as int,
-      style: null == style
-          ? _self.style
-          : style // ignore: cast_nullable_to_non_nullable
-              as WrestlingStyle,
-      suffix: freezed == suffix
-          ? _self.suffix
-          : suffix // ignore: cast_nullable_to_non_nullable
-              as String?,
-      unit: null == unit
-          ? _self.unit
-          : unit // ignore: cast_nullable_to_non_nullable
-              as WeightUnit,
-    ));
-  }
+/// Create a copy of WeightClass
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = freezed,Object? weight = null,Object? style = null,Object? suffix = freezed,Object? unit = null,}) {
+  return _then(_WeightClass(
+id: freezed == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int?,weight: null == weight ? _self.weight : weight // ignore: cast_nullable_to_non_nullable
+as int,style: null == style ? _self.style : style // ignore: cast_nullable_to_non_nullable
+as WrestlingStyle,suffix: freezed == suffix ? _self.suffix : suffix // ignore: cast_nullable_to_non_nullable
+as String?,unit: null == unit ? _self.unit : unit // ignore: cast_nullable_to_non_nullable
+as WeightUnit,
+  ));
+}
+
+
 }
 
 // dart format on
