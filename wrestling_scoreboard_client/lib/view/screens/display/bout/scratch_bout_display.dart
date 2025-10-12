1b338cc38fea9bea3b8b8f2d69883188ebdc78cf
Gustl22 <git@reb0.org>
2025-10-12 22:03:59 +0200

    fix: Avoid reloading whole screen on scratch bout update

--- wrestling_scoreboard_client/lib/view/screens/display/bout/scratch_bout_display.dart
+++ wrestling_scoreboard_client/lib/view/screens/display/bout/scratch_bout_display.dart
@@ -32,83 +32,75 @@ class _ScratchBoutDisplayState extends State<ScratchBoutDisplay> {
     return SingleConsumer<ScratchBout>(
       id: 0,
       builder: (context, scratchBout) {
-        return SingleConsumer<Bout>(
-          id: scratchBout.bout.id,
-          initialData: scratchBout.bout,
-          builder: (context, bout) {
-            return SingleConsumer<BoutConfig>(
-              id: scratchBout.boutConfig.id,
-              initialData: scratchBout.boutConfig,
-              builder: (context, boutConfig) {
-                return ManyConsumer<BoutResultRule, BoutConfig>(
-                  filterObject: boutConfig,
-                  builder: (context, boutResultRules) {
-                    return Consumer(
-                      builder: (context, ref, child) {
-                        return BoutScreen(
-                          wrestlingEvent: ScratchEvent(),
-                          officials: {},
-                          boutConfig: boutConfig,
-                          boutRules: boutResultRules,
-                          bouts: [bout],
-                          boutIndex: 0,
-                          bout: bout,
-                          actions: [
-                            ResponsiveScaffoldActionItem(
-                              icon: const Icon(Icons.restore_page),
-                              onTap: () async {
-                                final result = await showOkCancelDialog(
-                                  context: context,
-                                  child: Text('${localizations.reset}?'),
+        return SingleConsumer<BoutConfig>(
+          id: scratchBout.boutConfig.id,
+          initialData: scratchBout.boutConfig,
+          builder: (context, boutConfig) {
+            return ManyConsumer<BoutResultRule, BoutConfig>(
+              filterObject: boutConfig,
+              builder: (context, boutResultRules) {
+                return Consumer(
+                  builder: (context, ref, child) {
+                    return BoutScreen(
+                      wrestlingEvent: ScratchEvent(),
+                      officials: {},
+                      boutConfig: boutConfig,
+                      boutRules: boutResultRules,
+                      bouts: [scratchBout.bout],
+                      boutIndex: 0,
+                      bout: scratchBout.bout,
+                      actions: [
+                        ResponsiveScaffoldActionItem(
+                          icon: const Icon(Icons.restore_page),
+                          onTap: () async {
+                            final result = await showOkCancelDialog(
+                              context: context,
+                              child: Text('${localizations.reset}?'),
+                            );
+                            if (result && context.mounted) {
+                              // Some hacky way to refresh the page:
+                              // - family providers such as manyDataStream cannot be invalidated that easily
+                              // - invalidating / refreshing the localDataProvider does not lead to refreshing its dependent providers / the Consumers unless they are actively loaded
+                              // - setState does not force the Consumers to be refreshed.
+                              context.pop();
+
+                              // Need to call invalidation between pop and navigating back, otherwise it will get saved while popping (data safety feature of BoutScreen).
+                              // Only reset entities which are displayed, but keep e.g. BoutResultRules
+                              WidgetsBinding.instance.addPostFrameCallback((_) async {
+                                await ref.read(localDataNotifierProvider<BoutAction>().notifier).setState([]);
+                                ref.invalidate(localDataNotifierProvider<BoutAction>());
+                                await ref.read(localDataNotifierProvider<AthleteBoutState>().notifier).setState([]);
+                                ref.invalidate(localDataNotifierProvider<AthleteBoutState>());
+                                await ref.read(localDataNotifierProvider<Bout>().notifier).setState([]);
+                                ref.invalidate(localDataNotifierProvider<Bout>());
+                                await ref.read(localDataNotifierProvider<ScratchBout>().notifier).setState([]);
+                                ref.invalidate(localDataNotifierProvider<ScratchBout>());
+
+                                // Invalidate stream providers
+                                ref.invalidate(singleDataStreamProvider<ScratchBout>(SingleProviderData(id: 0)));
+                                ref.invalidate(singleDataStreamProvider<AthleteBoutState>(SingleProviderData(id: 0)));
+                                ref.invalidate(singleDataStreamProvider<AthleteBoutState>(SingleProviderData(id: 1)));
+                                ref.invalidate(singleDataStreamProvider<Bout>(SingleProviderData(id: 0)));
+                                ref.invalidate(
+                                  manyDataStreamProvider<BoutAction, Bout>(
+                                    ManyProviderData(filterObject: scratchBout.bout),
+                                  ),
                                 );
-                                if (result && context.mounted) {
-                                  // Some hacky way to refresh the page:
-                                  // - family providers such as manyDataStream cannot be invalidated that easily
-                                  // - invalidating / refreshing the localDataProvider does not lead to refreshing its dependent providers / the Consumers unless they are actively loaded
-                                  // - setState does not force the Consumers to be refreshed.
-                                  context.pop();
+                                ref.invalidate(singleDataStreamProvider<ScratchBout>(SingleProviderData(id: 0)));
 
-                                  // Need to call invalidation between pop and navigating back, otherwise it will get saved while popping (data safety feature of BoutScreen).
-                                  // Only reset entities which are displayed, but keep e.g. BoutResultRules
-                                  WidgetsBinding.instance.addPostFrameCallback((_) async {
-                                    await ref.read(localDataNotifierProvider<BoutAction>().notifier).setState([]);
-                                    ref.invalidate(localDataNotifierProvider<BoutAction>());
-                                    await ref.read(localDataNotifierProvider<AthleteBoutState>().notifier).setState([]);
-                                    ref.invalidate(localDataNotifierProvider<AthleteBoutState>());
-                                    await ref.read(localDataNotifierProvider<Bout>().notifier).setState([]);
-                                    ref.invalidate(localDataNotifierProvider<Bout>());
-                                    await ref.read(localDataNotifierProvider<ScratchBout>().notifier).setState([]);
-                                    ref.invalidate(localDataNotifierProvider<ScratchBout>());
-
-                                    // Invalidate stream providers
-                                    ref.invalidate(singleDataStreamProvider<ScratchBout>(SingleProviderData(id: 0)));
-                                    ref.invalidate(
-                                      singleDataStreamProvider<AthleteBoutState>(SingleProviderData(id: 0)),
-                                    );
-                                    ref.invalidate(
-                                      singleDataStreamProvider<AthleteBoutState>(SingleProviderData(id: 1)),
-                                    );
-                                    ref.invalidate(singleDataStreamProvider<Bout>(SingleProviderData(id: 0)));
-                                    ref.invalidate(
-                                      manyDataStreamProvider<BoutAction, Bout>(ManyProviderData(filterObject: bout)),
-                                    );
-                                    ref.invalidate(singleDataStreamProvider<ScratchBout>(SingleProviderData(id: 0)));
-
-                                    if (context.mounted) {
-                                      ScratchBoutDisplay.navigateTo(context);
-                                    }
-                                  });
+                                if (context.mounted) {
+                                  ScratchBoutDisplay.navigateTo(context);
                                 }
-                              },
-                              label: localizations.reset,
-                            ),
-                          ],
-                          navigateToBoutByIndex: (context, index) {
-                            context.pop();
+                              });
+                            }
                           },
-                          weightClass: scratchBout.weightClass,
-                        );
+                          label: localizations.reset,
+                        ),
+                      ],
+                      navigateToBoutByIndex: (context, index) {
+                        context.pop();
                       },
+                      weightClass: scratchBout.weightClass,
                     );
                   },
                 );
