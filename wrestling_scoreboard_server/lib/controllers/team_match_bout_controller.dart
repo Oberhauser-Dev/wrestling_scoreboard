e69eba97716539ede41c039b040f3829252e5df3
Gustl22 <git@reb0.org>
2026-01-26 18:34:03 +0100

    feat: Set team match result, if every bout has a result

--- wrestling_scoreboard_server/lib/controllers/team_match_bout_controller.dart
+++ wrestling_scoreboard_server/lib/controllers/team_match_bout_controller.dart
@@ -1,12 +1,17 @@
+import 'package:shelf/shelf.dart';
 import 'package:wrestling_scoreboard_common/common.dart';
 import 'package:wrestling_scoreboard_server/controllers/bout_controller.dart';
 import 'package:wrestling_scoreboard_server/controllers/common/orderable_controller.dart';
 import 'package:wrestling_scoreboard_server/controllers/common/organizational_controller.dart';
 import 'package:wrestling_scoreboard_server/controllers/common/shelf_controller.dart';
+import 'package:wrestling_scoreboard_server/controllers/team_lineup_controller.dart';
+import 'package:wrestling_scoreboard_server/controllers/team_match_controller.dart';
 
 class TeamMatchBoutController extends ShelfController<TeamMatchBout>
     with OrganizationalController<TeamMatchBout>, OrderableController<TeamMatchBout> {
   static final TeamMatchBoutController _singleton = TeamMatchBoutController._internal();
+
+  TeamMatchBoutController._internal() : super();
 
   factory TeamMatchBoutController() {
     return _singleton;
@@ -29,5 +34,41 @@ class TeamMatchBoutController extends ShelfController<TeamMatchBout>
     );
   }
 
-  TeamMatchBoutController._internal() : super();
+  @override
+  Future<Response> handlePostRequestSingle(Map<String, Object?> json) async {
+    final updatedTeamMatchBout = parseSingleJson<TeamMatchBout>(json);
+    final obfuscate = false;
+
+    // Update team match result, if every bout has a result.
+    if (updatedTeamMatchBout.bout.result != null && updatedTeamMatchBout.teamMatch.id != null) {
+      final teamMatchBouts = await TeamMatchBoutController().getByTeamMatch(
+        updatedTeamMatchBout.teamMatch.id!,
+        obfuscate: obfuscate,
+      );
+      if (teamMatchBouts.every((tmb) => tmb.bout.result != null)) {
+        final teamMatch = await TeamMatchController().getSingle(
+          updatedTeamMatchBout.teamMatch.id!,
+          obfuscate: obfuscate,
+        );
+        if (teamMatch.resultRole == null ||
+            teamMatch.home.classificationPoints == null ||
+            teamMatch.guest.classificationPoints == null) {
+          final homeClassificationPoints = TeamMatch.getClassificationPoints(teamMatchBouts.map((e) => e.bout.r));
+          final guestClassificationPoints = TeamMatch.getClassificationPoints(teamMatchBouts.map((e) => e.bout.b));
+          if (homeClassificationPoints > 0 || guestClassificationPoints > 0) {
+            final home = teamMatch.home.copyWith(classificationPoints: homeClassificationPoints);
+            final guest = teamMatch.guest.copyWith(classificationPoints: guestClassificationPoints);
+            final resultRole = MatchResultRole.fromDiff(homeClassificationPoints - guestClassificationPoints);
+            final endDate = teamMatch.endDate ?? MockableDateTime.now().toUtc();
+            await TeamLineupController().updateSingle(home);
+            await TeamLineupController().updateSingle(guest);
+            await TeamMatchController().updateSingle(
+              teamMatch.copyWith(resultRole: resultRole, home: home, guest: guest, endDate: endDate),
+            );
+          }
+        }
+      }
+    }
+    return super.handlePostRequestSingle(json);
+  }
 }
